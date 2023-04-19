59e83e18709d14b7ce3b2d6886c2b2585e8e3f58
pykrete <landysixten@gmail.com>
2023-04-19 13:50:17 +0200

    Rename Main.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
     import Header from './components/Header.vue'
-    import Match from './components/Match.vue'
+    import Main from './components/Main.vue'
 </script>
 
 <template>
     <Header />
+    <Main />
 </template>
 
 <style scoped>
