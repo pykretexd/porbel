4950a2e02bd2549a65aed196806679b93500656e
pykrete <landysixten@gmail.com>
2023-04-19 14:55:44 +0200

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,9 +2,9 @@
     <header>
     <nav class="px-4 lg:px-6 py-2.5">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" class="flex items-center">
+            <div class="flex items-center">
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Porbel</span>
-            </a>
+            </div>
             <div class="flex items-center lg:order-2">
                 <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
                 <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Sign up</a>
